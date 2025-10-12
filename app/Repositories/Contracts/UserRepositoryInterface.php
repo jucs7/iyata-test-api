f5262343ed12d38c36ce29f273c6d6b8e83ae10e
jucs7 <jucs0709@gmail.com>
2025-10-12 00:09:23 -0500

    feat(repositories): update User repository contract

--- app/Repositories/Contracts/UserRepositoryInterface.php
+++ app/Repositories/Contracts/UserRepositoryInterface.php
@@ -5,7 +5,11 @@ namespace App\Repositories\Contracts;
 use App\Models\User;
 
 interface UserRepositoryInterface
-{
-    public function create(array $data): User;
+{   
+    public function all(): array;
+    public function find(int $id): ?User;
     public function findByEmail(string $email): ?User;
+    public function create(array $data): User;
+    public function update(User $user, array $data): User;
+    public function delete(User $user): void;
 }
