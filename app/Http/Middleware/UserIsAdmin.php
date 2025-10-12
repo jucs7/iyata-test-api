0dec4fe299236d7d860d88d35006d37c79a775de
jucs7 <jucs0709@gmail.com>
2025-10-12 10:23:51 -0500

    feat(Middleware): add user is admin middleware

---
+++ app/Http/Middleware/UserIsAdmin.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Auth;
+
+class UserIsAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (!Auth::check() || !Auth::user()->isAdmin()) {
+            return response()->json([
+                "error_id" => 403,
+                "message" => "You don't have permission to do this operation.",
+            ]);
+        }
+        
+        return $next($request);
+    }
+}
