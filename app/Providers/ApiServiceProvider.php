e7396be34edaabec2f70dfdadb55b3a688ad2abe
jucs7 <jucs0709@gmail.com>
2025-10-12 15:42:20 -0500

    feat(ApiServiceProvider): add Api services for CRUD

--- app/Providers/ApiServiceProvider.php
+++ app/Providers/ApiServiceProvider.php
@@ -2,8 +2,14 @@
 
 namespace App\Providers;
 
+use App\Repositories\Contracts\CategoryRepositoryInterface;
+use App\Repositories\Contracts\ProductRepositoryInterface;
+use App\Repositories\Contracts\SupplierRepositoryInterface;
 use App\Repositories\Contracts\UserRepositoryInterface;
 use App\Services\AuthService;
+use App\Services\CategoryService;
+use App\Services\ProductService;
+use App\Services\SupplierService;
 use Illuminate\Support\ServiceProvider;
 
 class ApiServiceProvider extends ServiceProvider
@@ -15,6 +21,18 @@ class ApiServiceProvider extends ServiceProvider
     {
         $this->app->singleton(AuthService::class, function ($app) {
             return new AuthService($app->make(UserRepositoryInterface::class));
+        });
+
+        $this->app->singleton(CategoryService::class, function ($app) {
+            return new CategoryService($app->make(CategoryRepositoryInterface::class));
+        });
+
+        $this->app->singleton(SupplierService::class, function ($app) {
+            return new SupplierService($app->make(SupplierRepositoryInterface::class));
+        });
+
+        $this->app->singleton(ProductService::class, function ($app) {
+            return new ProductService($app->make(ProductRepositoryInterface::class));
         });
     }
 
